af0eca07c41a71166bcba08e62b6420483ec1e3a
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-09-29 10:38:06 +0000

    update allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -36,7 +36,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = ['jfonei-django-todo-app.herokuapp.com']
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
